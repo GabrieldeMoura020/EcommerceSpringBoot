1adc1d15c106a421518217bccbf208a4615f1c89
GABRIEL ROCHADEL DE MOURA <gabriel.moura@alunos.sc.senac.br>
2023-10-09 21:44:15 -0300

    Ajustando o codigo 2

--- src/main/java/com/ecommerce/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/ecommerce/ecommerce/controller/UsuarioController.java
@@ -1,10 +1,14 @@
 package com.ecommerce.ecommerce.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +23,9 @@ public class UsuarioController {
     @Autowired
     UsuarioRepository usuarioRepositorio;
 
-    @GetMapping(value = "/")
-    public void get(){
-
+    @GetMapping(value = "/usuario/listar")
+    public List<UsuarioEntity> getMethodName(){
+        return usuarioRepositorio.findAll();
     }
 
     @PostMapping(value = "/usuario")
@@ -30,5 +34,9 @@ public class UsuarioController {
         return new ResponseEntity<>(_user, HttpStatus.CREATED);
     }
 
-    
+    @DeleteMapping(path = {"/usuario/{id}"})
+    public ResponseEntity <?> deletar(@PathVariable long id) {
+        usuarioRepositorio.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
 }
